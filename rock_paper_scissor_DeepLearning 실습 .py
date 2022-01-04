e91d6d200dda4e3ce32ee4935269ae2f54075e9a
Seona Kim <pinotnoir056@outlook.com>
2022-01-04 17:08:03 +0900

    rock_paper_scissor

---
+++ rock_paper_scissor_DeepLearning 실습 .py
@@ -0,0 +1,191 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from PIL import Image
+import glob
+import os
+
+print("PIL 라이브러리 import 완료!")
+
+
+# In[2]:
+
+
+def resize_images(img_path):
+	images=glob.glob(img_path + "/*.jpg")  
+    
+	print(len(images), " images to be resized.")
+
+    # 파일마다 모두 28x28 사이즈로 바꾸어 저장합니다.
+	target_size=(28,28)
+	for img in images:
+		old_img=Image.open(img)
+		new_img=old_img.resize(target_size,Image.ANTIALIAS)
+		new_img.save(img, "JPEG")
+    
+	print(len(images), " images resized.")
+	
+# 가위 이미지가 저장된 디렉토리 아래의 모든 jpg 파일을 읽어들여서
+image_dir_path = os.getenv("HOME") + "/aiffel/rock_scissor_paper/test/rock"
+resize_images(image_dir_path)
+
+print("샘플 rock이미지 resize 완료!")
+
+
+# In[3]:
+
+
+import numpy as np
+import os
+
+def load_data(img_path, number_of_data=314):  # 가위바위보 이미지 개수 총합에 주의하세요.
+    # 가위 : 0, 바위 : 1, 보 : 2
+    img_size=28
+    color=3
+    #이미지 데이터와 라벨(가위 : 0, 바위 : 1, 보 : 2) 데이터를 담을 행렬(matrix) 영역을 생성합니다.
+    imgs=np.zeros(number_of_data*img_size*img_size*color,dtype=np.int32).reshape(number_of_data,img_size,img_size,color)
+    labels=np.zeros(number_of_data,dtype=np.int32)
+
+    idx=0
+    for file in glob.iglob(img_path+'/scissor/*.jpg'):
+        img = np.array(Image.open(file),dtype=np.int32)
+        imgs[idx,:,:,:]=img    # 데이터 영역에 이미지 행렬을 복사
+        labels[idx]=0   # 가위 : 0
+        idx=idx+1
+
+    for file in glob.iglob(img_path+'/rock/*.jpg'):
+        img = np.array(Image.open(file),dtype=np.int32)
+        imgs[idx,:,:,:]=img    # 데이터 영역에 이미지 행렬을 복사
+        labels[idx]=1   # 바위 : 1
+        idx=idx+1  
+    
+    for file in glob.iglob(img_path+'/paper/*.jpg'):
+        img = np.array(Image.open(file),dtype=np.int32)
+        imgs[idx,:,:,:]=img    # 데이터 영역에 이미지 행렬을 복사
+        labels[idx]=2   # 보 : 2
+        idx=idx+1
+        
+    print("학습데이터(x_train)의 이미지 개수는", idx,"입니다.")
+    return imgs, labels
+
+image_dir_path = os.getenv("HOME") + "/aiffel/rock_scissor_paper"
+(x_train, y_train)=load_data(image_dir_path)
+x_train_norm = x_train/255.0   # 입력은 0~1 사이의 값으로 정규화
+x_train_reshaped = x_train_norm.reshape( -1, 28, 28, 3)  # 데이터갯수에 -1을 쓰면 reshape시 자동계산됩니다.
+
+print("x_train shape: {}".format(x_train.shape))
+print("y_train shape: {}".format(y_train.shape))
+
+
+# In[4]:
+
+
+plt.imshow(x_train[0])
+print('라벨: ', y_train[0])
+
+
+# In[13]:
+
+
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+
+# model을 직접 만들어 보세요.
+# Hint! model의 입력/출력부에 특히 유의해 주세요. 가위바위보 데이터셋은 MNIST 데이터셋과 어떤 점이 달라졌나요?
+# [[YOUR CODE]]
+
+n_channel_1=64
+n_channel_2=128
+n_dense=32
+
+model=keras.models.Sequential()
+model.add(keras.layers.Conv2D(n_channel_1, (3,3), activation='relu', input_shape=(28,28,3)))
+model.add(keras.layers.MaxPool2D(2,2))
+model.add(keras.layers.Conv2D(n_channel_2, (3,3), activation='relu'))
+model.add(keras.layers.MaxPooling2D((2,2)))
+model.add(keras.layers.Flatten())
+model.add(keras.layers.Dense(n_dense, activation='relu'))
+model.add(keras.layers.Dense(10, activation='softmax'))
+
+model.summary()
+model.compile(optimizer='adam',
+             loss='sparse_categorical_crossentropy',
+             metrics=['accuracy'])
+
+
+# In[14]:
+
+
+model.fit(x_train_reshaped, y_train, epochs=30, validation_split=0.3)
+
+
+# In[15]:
+
+
+# x_test, y_test를 만드는 방법은 x_train, y_train을 만드는 방법과 아주 유사합니다.
+# [[YOUR CODE]]
+
+import numpy as np
+
+def load_data(img_path, number_of_data=334):  # 가위바위보 이미지 개수 총합에 주의하세요.
+    # 가위 : 0, 바위 : 1, 보 : 2
+    img_size=28
+    color=3
+    #이미지 데이터와 라벨(가위 : 0, 바위 : 1, 보 : 2) 데이터를 담을 행렬(matrix) 영역을 생성합니다.
+    imgs=np.zeros(number_of_data*img_size*img_size*color,dtype=np.int32).reshape(number_of_data,img_size,img_size,color)
+    labels=np.zeros(number_of_data,dtype=np.int32)
+
+    idx=0
+    for file in glob.iglob(img_path+'/scissor/*.jpg'):
+        img = np.array(Image.open(file),dtype=np.int32)
+        imgs[idx,:,:,:]=img    # 데이터 영역에 이미지 행렬을 복사
+        labels[idx]=0   # 가위 : 0
+        idx=idx+1
+
+    for file in glob.iglob(img_path+'/rock/*.jpg'):
+        img = np.array(Image.open(file),dtype=np.int32)
+        imgs[idx,:,:,:]=img    # 데이터 영역에 이미지 행렬을 복사
+        labels[idx]=1   # 바위 : 1
+        idx=idx+1  
+    
+    for file in glob.iglob(img_path+'/paper/*.jpg'):
+        img = np.array(Image.open(file),dtype=np.int32)
+        imgs[idx,:,:,:]=img    # 데이터 영역에 이미지 행렬을 복사
+        labels[idx]=2   # 보 : 2
+        idx=idx+1
+        
+    print("학습데이터(x_train)의 이미지 개수는", idx,"입니다.")
+    return imgs, labels
+
+image_dir_path = os.getenv("HOME") + "/aiffel/rock_scissor_paper/test"
+(x_test, y_test)=load_data(image_dir_path)
+x_test_norm = x_test/255.0   # 입력은 0~1 사이의 값으로 정규화
+
+print("x_test shape: {}".format(x_test.shape))
+print("y_test shape: {}".format(y_test.shape))
+
+x_test_reshaped = x_test_norm.reshape( -1, 28, 28, 3)
+
+
+# In[16]:
+
+
+# 모델 평가
+model.evaluate(x_test_reshaped, y_test)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
